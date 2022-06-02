08909b84840b8f1159a708c788abf6f6f6ee4e2b
Raymond Serion <rjserion@gmail.com>
2022-06-02 08:16:58 +0800

    Format HTML for Custom Template
    Added html doctype and fix html format by adding head and enclosing html element inside body tag properly

--- iona-plugin/templates/frontpage.php
+++ iona-plugin/templates/frontpage.php
@@ -2,22 +2,28 @@
 /**
  * Front Page Custom Template
  */
-wp_head();
-
 ?>
-<div id="wrapper">
-    <h1 class="page-title">Cat Browser</h1>
-    <form id="frmCatBreedLoader">
-        <div class="form-group">
-            <label>Breed</label>
-            <select id="catBreedSelect" required disabled>
-                <option value="">Select Breed</option>
-            </select>
-        </div>
-    </form>
-    <div id="listArea"></div>
-    <button id="btnLoadMore" class="hidden">Load More</button>
-</div>
-<?php
-wp_footer();
-?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="wrapper">
+        <h1 class="page-title">Cat Browser</h1>
+        <form id="frmCatBreedLoader">
+            <div class="form-group">
+                <label>Breed</label>
+                <select id="catBreedSelect" required disabled>
+                    <option value="">Select Breed</option>
+                </select>
+            </div>
+        </form>
+        <div id="listArea"></div>
+        <button id="btnLoadMore" class="hidden">Load More</button>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+</html>
--- iona-plugin/templates/single.php
+++ iona-plugin/templates/single.php
@@ -2,23 +2,29 @@
 /**
  * Single Page Custom Template
  */
-wp_head();
-
 ?>
-<div id="wrapper">
-    <div class="form-group">
-        <a href="<?php echo $breedInfo != null ? home_url("/?breed=" . $breedInfo->breeds[0]->id) : home_url(); ?>">Back</a>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="wrapper">
+        <div class="form-group">
+            <a href="<?php echo $breedInfo != null ? home_url("/?breed=" . $breedInfo->breeds[0]->id) : home_url(); ?>">Back</a>
+        </div>
+        <?php if($breedInfo == null) { ?>
+            <p>There are no data associated with the given info</p>
+        <?php } else { ?>
+            <img class="img-single" src="<?php echo $breedInfo->url; ?>" />
+            <h2 class="breed-name">Name: <?php echo $breedInfo->breeds[0]->name; ?></h2>
+            <h3 class="breed-origin">Origin: <?php echo $breedInfo->breeds[0]->origin; ?></h3>
+            <h3 class="breed-temperament">Temperament: <?php echo $breedInfo->breeds[0]->temperament; ?></h3>
+            <p class="breed-description"><?php echo $breedInfo->breeds[0]->description; ?></p>
+        <?php }  ?>
     </div>
-    <?php if($breedInfo == null) { ?>
-        <p>There are no data associated with the given info</p>
-    <?php } else { ?>
-        <img class="img-single" src="<?php echo $breedInfo->url; ?>" />
-        <h2 class="breed-name">Name: <?php echo $breedInfo->breeds[0]->name; ?></h2>
-        <h3 class="breed-origin">Origin: <?php echo $breedInfo->breeds[0]->origin; ?></h3>
-        <h3 class="breed-temperament">Temperament: <?php echo $breedInfo->breeds[0]->temperament; ?></h3>
-        <p class="breed-description"><?php echo $breedInfo->breeds[0]->description; ?></p>
-    <?php }  ?>
-</div>
-<?php
-wp_footer();
-?>
+    <?php wp_footer(); ?>
+</body>
+</html>
